d0c7b6edacdf372379d370022ef17d99f6700e03
Lukasz Kalabis <lukkalab88@gmail.com>
2020-10-13 15:18:50 +0200

    update week_IV

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-alert("ELOSZKA PIERDZIOSZKA!!!");
+const firstName = 'Lukasz';
+const age = 32;
+
+console.log(firstName);
+console.log(`Witaj Drogi Odwiedzajacy, nazywam sie ${firstName} i mam ${age} lata.`);
+
+const heading = document.querySelector(`.main-heading__link--js`);
